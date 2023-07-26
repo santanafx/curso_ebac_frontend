bfd27617158c32696093d4e59aabaafbb87ad052
Lucas Santana <santanafx@hotmail.com>
2023-07-26 18:38:07 -0300

    feat: inclusão do exercicio

---
+++ html-js/main.js
@@ -0,0 +1,46 @@
+const campoA = document.getElementById("campoA");
+const campoB = document.getElementById("campoB");
+const form = document.querySelector(".formNumeros");
+
+function validaFormularioFuncao(campoA, campoB) {
+    const inputValidacaoCampoA = document.querySelector(
+        ".inputValidacaoCampoA"
+    );
+
+    const inputValidacaoCampoB = document.querySelector(
+        ".inputValidacaoCampoB"
+    );
+
+    if (campoB.value > campoA.value) {
+        inputValidacaoCampoA.style.border = "2px solid green";
+        inputValidacaoCampoB.style.border = "2px solid green";
+    } else {
+        inputValidacaoCampoA.style.border = "2px solid red";
+        inputValidacaoCampoB.style.border = "2px solid red";
+    }
+}
+
+function mensagemValidacaoFuncao(campoA, campoB) {
+    const mensagemValido = `O número do campo B: <b>${campoB.value}</b> é maior que o número do campo A: <b>${campoA.value}</b> `;
+
+    const mensagemInvalido = `O número do campo B: <b>${campoB.value}</b> <b>não</b> é maior que o número do campo A: <b>${campoA.value}</b> `;
+
+    const mensagemValidacao = document.querySelector(".mensagemValidacao");
+
+    const campoMensagem = document.querySelector(".campoMensagem");
+
+    if (campoB.value > campoA.value) {
+        mensagemValidacao.innerHTML = mensagemValido;
+        campoMensagem.style.background = "green";
+    } else {
+        mensagemValidacao.innerHTML = mensagemInvalido;
+        campoMensagem.style.background = "red";
+    }
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    validaFormularioFuncao(campoA, campoB);
+    mensagemValidacaoFuncao(campoA, campoB);
+});
